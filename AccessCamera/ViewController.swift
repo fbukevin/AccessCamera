cc7fe9dbffb28c064f8d4b83028b05a61517b033
yagobonardi <yagobonardi@gmail.com>
2020-11-12 02:02:54 -0300

    fix code and build configurations to run project with swift 5 in macOS 10.15

--- AccessCamera/ViewController.swift
+++ AccessCamera/ViewController.swift
@@ -1,74 +1,79 @@
-//
-//  ViewController.swift
-//  openCamera
-//
-//  Created by Veck on 2017/1/5.
-//  Copyright © 2017年 Sanity. All rights reserved.
-//
+    //
+    //  ViewController.swift
+    //  openCamera
+    //
+    //  Created by Veck on 2017/1/5.
+    //  Copyright © 2017年 Sanity. All rights reserved.
+    //
 
-import Cocoa
-import AVFoundation
+    import Cocoa
+    import AVFoundation
 
-class ViewController: NSViewController {
-    
-    @IBOutlet weak var camera: NSView!
-    
-    let captureSession = AVCaptureSession()
-    var captureDevice : AVCaptureDevice?
-    var previewLayer : AVCaptureVideoPreviewLayer?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    class ViewController: NSViewController {
         
-        camera.layer = CALayer()
+        @IBOutlet weak var camera: NSView!
         
+        let captureSession = AVCaptureSession()
+        var captureDevice : AVCaptureDevice?
+        var previewLayer : AVCaptureVideoPreviewLayer?
         
-        // Do any additional setup after loading the view, typically from a nib.
-        captureSession.sessionPreset = AVCaptureSessionPresetLow
-        
-        // Get all audio and video devices on this machine
-        let devices = AVCaptureDevice.devices()
-        
-        // Find the FaceTime HD camera object
-        for device in devices! {
-            print(device)
+        override func viewDidLoad() {
+            super.viewDidLoad()
             
-            // Camera object found and assign it to captureDevice
-            if ((device as AnyObject).hasMediaType(AVMediaTypeVideo)) {
+            camera.layer = CALayer()
+            
+            
+            // Do any additional setup after loading the view, typically from a nib.
+            captureSession.sessionPreset = AVCaptureSession.Preset.low
+            
+            let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(
+                deviceTypes: [ .builtInWideAngleCamera, .externalUnknown ],
+                mediaType: .video,
+                position: .unspecified
+            )
+            
+            let devices = deviceDiscoverySession.devices
+            
+            // Find the FaceTime HD camera object
+            
+            for device in devices {
                 print(device)
-                captureDevice = device as? AVCaptureDevice
+                
+                // Camera object found and assign it to captureDevice
+                if ((device as AnyObject).hasMediaType(AVMediaType.video)) {
+                    print(device)
+                    captureDevice = device as AVCaptureDevice
+                }
             }
-        }
-        
-        
-        if captureDevice != nil {
             
-            do {
+            
+            if captureDevice != nil {
                 
-                try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
-                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-                previewLayer?.frame = (self.camera.layer?.frame)!
-                
-                // Add previewLayer into custom view
-                self.camera.layer?.addSublayer(previewLayer!)
-                
-                // Start camera
-                captureSession.startRunning()
-                
-            } catch {
-                print(AVCaptureSessionErrorKey.description)
+                do {
+                    
+                    try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice!))
+                    previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                    previewLayer?.frame = (self.camera.layer?.frame)!
+                    
+                    // Add previewLayer into custom view
+                    self.camera.layer?.addSublayer(previewLayer!)
+                    
+                    // Start camera
+                    captureSession.startRunning()
+                    
+                } catch {
+                    print(AVCaptureSessionErrorKey.description)
+                }
+            }
+            
+            
+        }
+        
+        override var representedObject: Any? {
+            didSet {
+                // Update the view, if already loaded.
             }
         }
         
         
     }
-    
-    override var representedObject: Any? {
-        didSet {
-            // Update the view, if already loaded.
-        }
-    }
-    
-    
-}
-
